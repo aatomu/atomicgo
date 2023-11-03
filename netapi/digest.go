7672796349dfba93afc476c9998076b298a42c93
aatomu <aatomu21263+git@gmail.com>
2023-11-04 03:16:20 +0900

    比較的最近ならInputをskipできるように

--- netapi/digest.go
+++ netapi/digest.go
@@ -2,37 +2,79 @@ package netapi
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
 	"errors"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"regexp"
+	"strconv"
+	"strings"
 	"time"
 )
 
 type DigestAuth struct {
-	realm string
-	nonce map[string]bool
+	realm    string
+	nonce    map[string]string // nonce="timestamp Hash(timestamp+randText)", nonce = {timestamp:"nonce"}
+	lifetime time.Duration
 }
 
-func DigestAuthNew(Realm string) *DigestAuth {
+func DigestAuthNew(Realm string, lifetime time.Duration) *DigestAuth {
 	return &DigestAuth{
-		realm: Realm,
-		nonce: map[string]bool{},
+		realm:    Realm,
+		nonce:    map[string]string{},
+		lifetime: lifetime,
 	}
 }
 
 // Send "Digest Auth" To Client
-func (d *DigestAuth) Require(w http.ResponseWriter, lifetime time.Duration) {
-	nonceText := fmt.Sprintf("%x%x", time.Now().UnixMicro(), time.Now().UnixNano())
-	d.nonce[nonceText] = false
+func (d *DigestAuth) Require(r *http.Request, w http.ResponseWriter) {
+	isUserInput := d.isUserInput(r)
+	// New Nonce
+	timestamp := fmt.Sprintf("%d", time.Now().UnixMicro())
+	randText := fmt.Sprintf("%x", rand.New(rand.NewSource(time.Now().UnixNano())).Int())
+	d.nonce[timestamp] = randText
 
-	w.Header().Set("WWW-Authenticate", fmt.Sprintf(`Digest realm="%s",nonce="%s",algorithm=MD5,qop="auth"`, d.realm, nonceText))
+	if isUserInput {
+		w.Header().Set("WWW-Authenticate", fmt.Sprintf(`Digest realm="%s",nonce="%s",algorithm=MD5,qop="auth"`, d.realm, newNonce(timestamp, randText)))
+	} else {
+		w.Header().Set("WWW-Authenticate", fmt.Sprintf(`Digest realm="%s",nonce="%s",stale=true,algorithm=MD5,qop="auth"`, d.realm, newNonce(timestamp, randText)))
+	}
 	w.WriteHeader(http.StatusUnauthorized)
 
+	// Delete Nonce
 	go func() {
-		time.Sleep(lifetime)
-		delete(d.nonce, nonceText)
+		time.Sleep(d.lifetime * 2)
+		delete(d.nonce, timestamp)
 	}()
+}
+
+// Bypass User Input (if nonce is stale)
+func (d *DigestAuth) isUserInput(r *http.Request) (isInvaild bool) {
+	// Exist Authorization Header
+	Auth := r.Header.Values("Authorization")
+	if len(Auth) != 1 {
+		return true
+	}
+
+	value := regexp.MustCompile(`nonce=\"(.+?)\"`).FindStringSubmatch(Auth[0])
+	// Exist Nonce Tag
+	if len(value) != 2 {
+		return true
+	}
+
+	content := strings.Split(value[1], " ")
+	// Available Tag
+	if len(content) != 2 {
+		return true
+	}
+
+	t, _ := strconv.ParseInt(content[0], 10, 64)
+	timestamp := time.UnixMicro(t)
+	// Time check
+	// timestamp > time.Now() - lifetime
+	isOld := time.Since(timestamp) > d.lifetime
+	return isOld
 }
 
 func (d *DigestAuth) GetUsername(r *http.Request) (username string) {
@@ -54,6 +96,7 @@ func (d DigestAuth) Checksum(user string, r *http.Request) (ok bool, err error) 
 	if len(Auth) != 1 {
 		return false, errors.New("authorization required")
 	}
+
 	// response = MD5(A1):nonce:nc:cnonce:qop:MD5(A2)
 	// A1(user) = username:realm:password
 	// A2 = Method:uri
@@ -87,9 +130,9 @@ func (d DigestAuth) Checksum(user string, r *http.Request) (ok bool, err error) 
 	}
 	response = value[1]
 
-	for nonce := range d.nonce {
+	for timestamp, randText := range d.nonce {
 		// Create Checksum Hash
-		checksumHash := fmt.Sprintf("%s:%s:%s:%s:auth:%s", user, nonce, nc, cnonce, A2)
+		checksumHash := fmt.Sprintf("%s:%s:%s:%s:auth:%s", user, newNonce(timestamp, randText), nc, cnonce, A2)
 		md5Hash = md5.Sum([]byte(checksumHash))
 		checksumHash = fmt.Sprintf("%x", md5Hash)
 		if response == checksumHash {
@@ -101,4 +144,9 @@ func (d DigestAuth) Checksum(user string, r *http.Request) (ok bool, err error) 
 
 func (d DigestAuth) NewUser(username, password string) (user string) {
 	return fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%s:%s:%s", username, d.realm, password))))
+}
+
+// nonce="timestamp Hash(timestamp+randText)"
+func newNonce(timestamp, randText string) (nonce string) {
+	return fmt.Sprintf("%s %x", timestamp, sha256.Sum256([]byte(timestamp+randText)))
 }
